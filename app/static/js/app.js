d11ef74779ffb514780f7c78ec9b8abd072727bf
beastboost <50527302+beastboost@users.noreply.github.com>
2026-08-15 19:42:47 +0100

    Load Music 2 player

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -8,4 +8,5 @@
     document.write(`<script src="js/subtitle-control.js?v=${encodeURIComponent(version)}"><\/script>`);
     document.write(`<script src="js/quality-control.js?v=${encodeURIComponent(version)}"><\/script>`);
     document.write(`<script src="js/device-control.js?v=${encodeURIComponent(version)}"><\/script>`);
+    document.write(`<script src="js/music2-player.js?v=${encodeURIComponent(version)}"><\/script>`);
 })();
