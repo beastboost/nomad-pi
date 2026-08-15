03549e13958a7201ae118f860aa44da1e7ef5672
beastboost <50527302+beastboost@users.noreply.github.com>
2026-08-15 18:57:01 +0100

    Load playback quality controls

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -5,4 +5,5 @@
     document.write(`<script src="js/playback-core.js?v=${encodeURIComponent(version)}"><\/script>`);
     document.write(`<script src="js/track-control.js?v=${encodeURIComponent(version)}"><\/script>`);
     document.write(`<script src="js/subtitle-control.js?v=${encodeURIComponent(version)}"><\/script>`);
+    document.write(`<script src="js/quality-control.js?v=${encodeURIComponent(version)}"><\/script>`);
 })();
